4ef82130197bc226ce2c79dac08e720145e572e6
stefania0610 <stefania.caporicci@gmail.com>
2022-09-16 12:03:37 +0200

    First Theme Commit

--- app/src/main/java/com/iarosinternational/corsoandroid/app/MainActivity.java
+++ app/src/main/java/com/iarosinternational/corsoandroid/app/MainActivity.java
@@ -3,9 +3,6 @@ package com.iarosinternational.corsoandroid.app;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
@@ -14,7 +11,6 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.iarosinternational.corsoandroid.app.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,13 +32,14 @@ public class MainActivity extends AppCompatActivity {
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_courses,
                 R.id.navigation_contacts, R.id.navigation_mission)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
     }
 
     @Override
