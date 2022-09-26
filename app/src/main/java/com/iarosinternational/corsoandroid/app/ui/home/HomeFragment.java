6080940689068f24dac0af923168cd68a152070f
emiya89 <110224670+emiya89@users.noreply.github.com>
2022-09-26 13:10:53 +0200

    wip Home recyclerview

--- app/src/main/java/com/iarosinternational/corsoandroid/app/ui/home/HomeFragment.java
+++ app/src/main/java/com/iarosinternational/corsoandroid/app/ui/home/HomeFragment.java
@@ -6,22 +6,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.iarosinternational.corsoandroid.app.JsonTask;
-import com.iarosinternational.corsoandroid.app.R;
 import com.iarosinternational.corsoandroid.app.databinding.FragmentHomeBinding;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment implements JsonTask.AsyncResponse {
 
     private FragmentHomeBinding binding;
-
+    private ArrayList<News> newsList;
     private final static String JSON_URL_NEWS = "https://raw.githubusercontent.com/tudo75/Iaros-International-App/main/resources/news_iaros.json";
+    private RecyclerView contenitore_news;
+    private NewsAdapter news_adapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -29,10 +38,23 @@ public class HomeFragment extends Fragment implements JsonTask.AsyncResponse {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        textView.setText(R.string.title_home);
+        newsList = new ArrayList<News>();
+
+        Log.e("Breakpoint Iaros App", "start JsonTask");
 
         new JsonTask(this).execute(JSON_URL_NEWS);
+
+        /*
+         1) recupero recyclerview inizializzo
+         2) Passareal recyclerview la lista popolo news
+         */
+        contenitore_news = binding.contenitoreNews;
+        news_adapter = new NewsAdapter(newsList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        contenitore_news.setLayoutManager(layoutManager);
+        contenitore_news.setItemAnimator(new DefaultItemAnimator());
+        //contenitore_news.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL));
+        contenitore_news.setAdapter(news_adapter);
 
         return root;
     }
@@ -53,6 +75,37 @@ public class HomeFragment extends Fragment implements JsonTask.AsyncResponse {
     public void processFinish(String result) {
         try {
             JSONArray news = new JSONArray(result);
+
+            final TextView debugTv = binding.debugTv;
+            debugTv.setText(result);
+
+            //Ciclo l'array json news
+            for(int i=0; i<news.length(); i++) {
+                //per ogni elemento dell'array inizializzo un oggetto News e setto tutti i suoi attributi
+                News tempNews = new News();
+                JSONObject news_json = (JSONObject) news.get(i);
+                String titolo_news = news_json.getString("titolo");
+                tempNews.setTitolo(titolo_news);
+                String corpo_mnews = news_json.getString("corpo");
+                tempNews.setCorpo(corpo_mnews);
+                String data_news = news_json.getString("data");
+                tempNews.setData(data_news);
+
+                //alla fine dell'inizializzazione inserisco l'oggetto News in un ArrayList
+                newsList.add(tempNews);
+            }
+
+            news_adapter.notifyDataSetChanged();
+
+            String debugNewsList = "";
+            for(int i=0; i < newsList.size(); i++) {
+                News tempNews = newsList.get(i);
+                String riga = "" + i + ": " + tempNews.getTitolo() + "\n";
+
+                debugNewsList += riga;
+            }
+
+            Log.e("Breakpoint Iaros App", "newsList created -> " + debugNewsList);
         } catch (JSONException e) {
             Log.e("JSON error", e.getMessage());
         }
---
+++ app/src/main/java/com/iarosinternational/corsoandroid/app/ui/home/NewsAdapter.java
@@ -0,0 +1,78 @@
+package com.iarosinternational.corsoandroid.app.ui.home;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.iarosinternational.corsoandroid.app.MainActivity;
+
+import com.iarosinternational.corsoandroid.app.databinding.ListaNewsBinding;
+import com.iarosinternational.corsoandroid.app.R;
+
+import java.util.ArrayList;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ListItemHolder> {
+
+    private ArrayList<News> listNews;
+    private ListaNewsBinding binding;
+
+    public NewsAdapter(ArrayList<News> arrayListNews){
+        listNews = arrayListNews;
+        Log.e("Breakpoint Iaros App", "NewsAdapter constructor");
+    }
+
+    @NonNull
+    @Override
+    public NewsAdapter.ListItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        //View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.lista_news, parent, false);
+
+        binding = ListaNewsBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+
+        Log.e("Breakpoint Iaros App", "NewsAdapter.ListItemHolder onCreateViewHolder");
+        return new ListItemHolder(binding.getRoot());
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListItemHolder holder, int position) {
+        News tempNews = listNews.get(position);
+
+        holder.tViewTitolo.setText(tempNews.getTitolo());
+
+        Log.e("Breakpoint Iaros App", "NewsAdapter onBindViewHolder");
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public class ListItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView tViewTitolo;
+        TextView tViewCorpo;
+        TextView tViewData;
+
+        public ListItemHolder(View view) {
+
+            super(view);
+            tViewTitolo = view.findViewById(R.id.textView_titolo);
+            tViewCorpo = view.findViewById(R.id.textView_corpo);
+            tViewData = view.findViewById(R.id.textView_data);
+
+            Log.e("Breakpoint Iaros App", "ListItemHolder constructor");
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+
+    }
+}
